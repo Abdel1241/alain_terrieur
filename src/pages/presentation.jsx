fea37a9f0162805359a6bb353fa7c2edcc67b2c8
Abdel1241 <119110660+Abdel1241@users.noreply.github.com>
2023-10-29 15:45:33 +0100

    Initial commit

--- src/pages/presentation.jsx
+++ src/pages/presentation.jsx
@@ -1,12 +1,10 @@
-import { Box, Flex, Text, Heading, Image, Spacer, Button, Link as ChakraLink, VStack, HStack } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading, Spacer, Button, Link as ChakraLink, VStack, HStack, Divider } from "@chakra-ui/react";
 import Link from 'next/link';
-import { CheckIcon, InfoIcon } from "@chakra-ui/icons";
-
 
 // Composant de la barre de navigation
 function NavBar() {
     return (
-        <Flex bg="white" p={5} shadow="md">
+        <Flex bg="white" p={5} shadow="xl">
             <Heading size="lg" fontWeight="semibold">
                 <Link href="/" passHref>
                     <ChakraLink>Alain Terrieur</ChakraLink>
@@ -31,37 +29,36 @@ function NavBar() {
 
 export default function Presentation() {
     return (
-        <Box bg="gray.100" minH="100vh">
+        <Box bg="gray.100" minH="100vh" p={10} >
             {/* Navigation */}
             <NavBar />
 
             {/* Main Content */}
-            <Flex direction="column" align="center" justify="center" py={20}>
-                <VStack spacing={10}>
-                    <Heading as="h1" size="2xl">
-                        Bienvenue chez Alain Terrieur
-                    </Heading>
-                    <Text fontSize="xl" maxW="container.md" textAlign="center">
-                        Spécialisé dans la rénovation et la décoration intérieure, Alain Terrieur vous offre une expérience inégalée pour transformer votre espace. Explorez notre expertise et découvrez pourquoi nous sommes les meilleurs dans le métier.
-                    </Text>
-                    <Image src="/path-to-image.jpg" alt="Exemple d'intérieur rénové" borderRadius="lg" shadow="lg" />
+            <VStack spacing={10} align="center" py={20}>
+                <Heading as="h1" size="2xl">
+                    Bienvenue chez Alain Terrieur
+                </Heading>
+                <Text fontSize="xl" maxW="container.md" textAlign="center">
+                    Spécialisé dans la rénovation et la décoration intérieure, Alain Terrieur vous offre une expérience inégalée pour transformer votre espace. Explorez notre expertise et découvrez pourquoi nous sommes les meilleurs dans le métier.
+                </Text>
+                
+                <Divider my={6} borderColor="gray.300" w="80%" />
 
-                    <Text fontSize="lg" maxW="container.sm" textAlign="center">
-                        Notre mission est de donner vie à vos visions. Chaque espace raconte une histoire, et nous sommes là pour aider cette histoire à se dérouler avec éclat.
-                    </Text>
-                </VStack>
+                <Text fontSize="lg" maxW="container.sm" textAlign="center">
+                    Notre mission est de donner vie à vos visions. Chaque espace raconte une histoire, et nous sommes là pour aider cette histoire à se dérouler avec éclat.
+                </Text>
 
                 {/* Contact Section */}
-                <VStack spacing={5} mt={20}>
+                <VStack spacing={5} mt={20} p={6} bg="white" shadow="lg" borderRadius="md" w="80%">
                     <Heading as="h2" size="xl">
                         Contactez-nous
                     </Heading>
                     <HStack spacing={5}>
-                        <ChakraLink href="mailto:contact@alainterrieur.com" isExternal>contact@alainterrieur.com</ChakraLink>
-                        <ChakraLink href="tel:+1234567890">+12 345 678 90</ChakraLink>
+                        <ChakraLink href="mailto:contact@alainterrieur.com" isExternal fontWeight="semibold">contact@alainterrieur.com</ChakraLink>
+                        <ChakraLink href="tel:+1234567890" fontWeight="semibold">+12 345 678 90</ChakraLink>
                     </HStack>
                 </VStack>
-            </Flex>
+            </VStack>
         </Box>
     );
 }
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,52 +1,46 @@
-import { Box, Flex, Text, Heading, Image, Stack, Button, Link as ChakraLink } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading, Stack, Button, Link as ChakraLink, Divider } from "@chakra-ui/react";
 import Link from 'next/link';
-import { CheckIcon, InfoIcon } from "@chakra-ui/icons";
-
 
 export default function Services() {
   const services = [
     {
-      title: "Aménagement d'intérieur:",
+      title: "Aménagement d'intérieur",
       description: "Notre équipe de spécialistes crée des espaces fonctionnels qui reflètent votre style personnel. De la consultation initiale à la réalisation finale, nous transformons vos visions en réalité.",
-      image: "/image1.jpg"
     },
     {
-      title: "Rénovation complète:",
+      title: "Rénovation complète",
       description: "Pour ceux qui cherchent à donner une nouvelle vie à leur espace, notre service de rénovation complète métamorphose votre intérieur du sol au plafond.",
-      image: "/image2.jpg" 
     },
     {
-      title: "Décoration sur mesure:",
+      title: "Décoration sur mesure",
       description: "Avec un accès à une vaste gamme de fournisseurs et de fabricants, nous personnalisons chaque détail, du choix des meubles aux traitements de fenêtre, pour correspondre parfaitement à votre style.",
-      image: "image3.jpg"
     },
     {
-      title: "Conseil en couleur:",
+      title: "Conseil en couleur",
       description: "Laissez nos experts vous guider à travers le monde complexe des couleurs pour trouver les teintes qui compléteront parfaitement votre espace et refléteront votre personnalité.",
-      image: "/image4.jpg"
     },
     {
-      title: "Éclairage d'ambiance:",
+      title: "Éclairage d'ambiance",
       description: "L'éclairage peut faire ou défaire un espace. Nos experts concevront un plan d'éclairage pour mettre en valeur les meilleures caractéristiques de votre maison tout en créant l'ambiance souhaitée.",
-      image: "/image5.jpg"
     },
     {
-      title: "Planification d'espace et Feng Shui:",
-      description: " Au-delà de la simple esthétique, nous utilisons des principes éprouvés pour améliorer le flux et l'énergie de votre espace, en garantissant une harmonie et un équilibre dans chaque pièce.",
-      image: "/image6.jpg"
+      title: "Planification d'espace et Feng Shui",
+      description: "Au-delà de la simple esthétique, nous utilisons des principes éprouvés pour améliorer le flux et l'énergie de votre espace, en garantissant une harmonie et un équilibre dans chaque pièce.",
     },
   ];
 
+
+
   return (
-    <Box bg="gray.100" minH="100vh">
+    <Box bg="gray.100" minH="100vh" p={5}>
       {/* Navigation */}
-      <Flex bg="white" p={5} shadow="md">
+      <Flex bg="white" p={5} shadow="xl" justify="space-between" borderRadius="md">
         <Heading size="lg" fontWeight="semibold">
           <Link href="/" passHref>
             <ChakraLink>Alain Terrieur</ChakraLink>
           </Link>
         </Heading>
-        <Flex ml="auto">
+        <Flex>
           <Link href="/presentation" passHref>
             <Button as={ChakraLink} mr={5} variant="outline">
               Présentation
@@ -60,16 +54,37 @@ export default function Services() {
         </Flex>
       </Flex>
 
+      {/* Title */}
+      <Heading mt={10} textAlign="center" size="2xl" fontWeight="bold">
+        Nos Services
+      </Heading>
+      <Text mt={2} textAlign="center" fontSize="xl">
+        Découvrez ce que nous offrons pour transformer votre espace
+      </Text>
+
       {/* Main Content */}
-      <Stack spacing={10} align="center" py={20}>
+      <Stack spacing={10} align="center" py={20} mt={5}>
         {services.map((service, index) => (
-          <Box key={index} p={5} shadow="md" borderWidth="1px" borderRadius="md">
-            <Heading fontSize="xl">{service.title}</Heading>
-            <Text mt={4}>{service.description}</Text>
-            <Image src={service.image} alt={`Image pour ${service.title}`} mt={5} borderRadius="md" />
+          <Box key={index} p={5} shadow="2xl" borderWidth="1px" borderRadius="md" maxW="700px" bg="white">
+            <Heading fontSize="xl" mb={4} textAlign="center">{service.title}</Heading>
+            <Divider mb={4} />
+            <Text>{service.description}</Text>
           </Box>
         ))}
+
+         {/* Contact Section */}
+         <Stack spacing={5} mt={20}>
+                    <Heading as="h2" size="xl">
+                        Contactez-nous
+                    </Heading>
+                    <Stack spacing={5}>
+                        <ChakraLink href="mailto:contact@alainterrieur.com" isExternal>contact@alainterrieur.com</ChakraLink>
+                        <ChakraLink href="tel:+1234567890">+12 345 678 90</ChakraLink>
+                    </Stack>
+                </Stack>
       </Stack>
     </Box>
   );
+
+  
 }
